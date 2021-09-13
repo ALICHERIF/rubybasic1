4787e9db37b34f4c07db1db76f82f0e160459bfa
System Administrator <root@MacBook-Pro-de-ALI.local>
2021-09-13 17:47:27 +0100

    first commit

---
+++ main.rb
@@ -0,0 +1,18 @@
+#puts "hello "
+#first_name= gets.chomp
+#puts("enter your favorite number #{first_name}")
+#favoritenum = gets.chomp
+#puts ("my favorite number is #{favoritenum * 2}")
+#string concatenation
+#for multiply by to you need simply change to integer
+#puts ("multiply number is #{favoritenum.to_i * 2}")
+#puts ("your reverse name is #{first_name.reverse}"+" "+ "you're lenght of your name is #{first_name.length}")
+#float a = 4.0
+#b = 10
+#puts b / a
+#puts "-" * 20
+#20.times {print "-"}
+#20.times {print rand(10)}
+#by default rand generate number between zero and one
+#puts 10 === "10".to_f
+#puts 10.eql?("10".to_f) #eql comparing the type 
